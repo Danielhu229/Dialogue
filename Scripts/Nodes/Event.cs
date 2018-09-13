b483c132330aec24f39656451241e4c70423467f
yiminghu <yimingdz@gmail.com>
2018-09-13 19:59:56 +0800

    specialze unity events

--- Scripts/Nodes/Event.cs
+++ Scripts/Nodes/Event.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using XNode;
-namespace Dialogue {
-	[NodeTint("#FFFFAA")]
-	public class Event : DialogueBaseNode {
-		[Input] public Connection input;
-		
-		public UnityEvent[] trigger; // Could use UnityEvent here, but UnityEvent has a bug that prevents it from serializing correctly on custom EditorWindows. So i implemented my own.
+namespace Dialogue
+{
+    [NodeTint("#FFFFAA")]
+    public class Event : DialogueBaseNode
+    {
+        [Input] public Connection input;
 
-		public override void Trigger() {
-			for (int i = 0; i < trigger.Length; i++) {
-				trigger[i].Invoke();
-			}
-		}
-	}
+        public VoidEvent[] trigger; // Could use UnityEvent here, but UnityEvent has a bug that prevents it from serializing correctly on custom EditorWindows. So i implemented my own.
+
+        public override void Trigger()
+        {
+            for (int i = 0; i < trigger.Length; i++)
+            {
+                trigger[i].Invoke();
+            }
+        }
+    }
 }
