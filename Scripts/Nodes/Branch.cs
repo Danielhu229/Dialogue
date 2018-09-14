f3354fca0eb26752ef11e2b3c05bb7cd76a19d80
yiminghu <yiminghu@tencent.com>
2018-09-14 12:00:30 +0800

    remove broken reference

--- Scripts/Nodes/Branch.cs
+++ Scripts/Nodes/Branch.cs
@@ -7,39 +7,45 @@ using UnityEngine;
 using UnityEngine.Events;
 using XNode;
 
-namespace Dialogue {
+namespace Dialogue
+{
     [NodeTint("#CCCCFF")]
-    public class Branch : DialogueBaseNode {
+    public class Branch : DialogueBaseNode
+    {
 
         [Input] public Chat.Connection input;
-        public Condition[] conditions;
+        // public Condition[] conditions;
         [Output] public Chat.Connection pass;
         [Output] public Chat.Connection fail;
 
         private bool success;
 
-        public override void Trigger() {
-            // Perform condition
-            bool success = true;
-            for (int i = 0; i < conditions.Length; i++) {
-                if (!conditions[i].Invoke()) {
-                    success = false;
-                    break;
-                }
-            }
+        public override void Trigger()
+        {
+            // // Perform condition
+            // bool success = true;
+            // for (int i = 0; i < conditions.Length; i++)
+            // {
+            //     if (!conditions[i].Invoke())
+            //     {
+            //         success = false;
+            //         break;
+            //     }
+            // }
 
-            //Trigger next nodes
-            NodePort port;
-            if (success) port = GetOutputPort("pass");
-            else port = GetOutputPort("fail");
-            if (port == null) return;
-            for (int i = 0; i < port.ConnectionCount; i++) {
-                NodePort connection = port.GetConnection(i);
-                (connection.node as DialogueBaseNode).Trigger();
-            }
+            // //Trigger next nodes
+            // NodePort port;
+            // if (success) port = GetOutputPort("pass");
+            // else port = GetOutputPort("fail");
+            // if (port == null) return;
+            // for (int i = 0; i < port.ConnectionCount; i++)
+            // {
+            //     NodePort connection = port.GetConnection(i);
+            //     (connection.node as DialogueBaseNode).Trigger();
+            // }
         }
     }
 
-    [Serializable]
-    public class Condition : SerializableCallback<bool> { }
+    // [Serializable]
+    // public class Condition : SerializableCallback<bool> { }
 }
